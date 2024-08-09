7f64535a4ce670097b94542876a1f7b90271445d
Yash <yash.verma@tailboost.ai>
2024-08-09 16:09:33 +0530

    add home page

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import homeimg from "../../assets/home.png";
 
 const index = () => {
   return (
-    <div className="flex justify-center max-sm:hidden items-center h-full bg-[#DAE5F5]">
-      hii
+    <div className="flex justify-center max-sm:hidden items-center h-full flex-col bg-[#DAE5F5]">
+      <img src={homeimg} className="w-2/3" alt="" />
+      <h1 className="text-3xl font-bold">Pocket Notes</h1>
+      <p className="text-center w-1/2">
+        Send and receive messages without keeping your phone online. Use Pocket
+        Notes on up to 4 linked devices and 1 mobile phone
+      </p>
     </div>
   );
 };
